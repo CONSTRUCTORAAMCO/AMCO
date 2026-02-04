09a6a539a30927e56d52a118c9695300bf7e7347
JuLian0705 <sabogaljulian27@gmail.com>
2026-02-03 20:24:19 -0500

    cambios en tyc #21

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,4 +1,4 @@
-import Formulario from "../assets/modules/formulario/formulario";
+import Formulario from "../assets/modules/formulario/Formulario.jsx";
 
 const Contacto = () => {
   return <Formulario />;

8bb232ba5af2918d93f52615b3240d9d3f34ed7b
JuLian0705 <sabogaljulian27@gmail.com>
2026-02-03 20:20:32 -0500

    cambios en tyc #20

--- src/assets/components/layout/MainLayout.jsx
+++ src/assets/components/layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { lazy, Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "../../components/footer/footer";
+import Footer from "../../components/Footer/Footer.jsx";
 import Navbar from "./navbar";
 const WhatsAppWidget = lazy(() => import("../../modules/whatsapp/whatsappwidget"));
 
